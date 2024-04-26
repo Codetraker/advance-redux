21092f48af489e29fa69d3182d8a172f7c7df4f4
Codetraker <swarajbibhuprasad@gmail.com>
2024-04-26 23:46:20 +0530

    working cart btn

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My 1st book',
+    description: 'My 1st book i ever wrote',
+  },
+  {
+    id: 'p2',
+    price: 10,
+    title: 'My 2nd book',
+    description: 'My 2nd book i ever wrote',
+  }
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+            <ProductItem
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              description={product.description}
+            />
+        ))}
       </ul>
     </section>
   );
